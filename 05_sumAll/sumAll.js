f8e565548bc7a90d88e82f672f7ad6aafdf02a0d
NinjazZz1 <85305843+NinjazZz1@users.noreply.github.com>
2024-07-21 22:49:24 +0100

    completed javascript exercises 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let sum = 0;
+    let minNumber = 0
+    let maxNumber = 0;
 
-};
+    if (num1 < num2) {
+        minNumber = num1;
+        maxNumber = num2;
+    } else {
+        minNumber = num2;
+        maxNumber = num1;
+    }
+
+    if (minNumber < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    } else {
+        for (let i = minNumber; i < maxNumber; i++) {
+            sum += i;
+        }
+        sum += maxNumber;
+        return sum;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let conversion = (fahrenheit - 32) * 5 / 9;
+  conversion = parseFloat(conversion.toFixed(1));
+  return conversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let conversion = (celsius * 9 / 5 + 32);
+  conversion = parseFloat(conversion.toFixed(1));
+  return conversion;
 };
 
 // Do not edit below this line
